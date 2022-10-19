317fe8b663aac7d44a04df000fd096e02d51e00b
Airomarto <airomarto@gmail.com>
2022-10-19 13:42:38 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * 'main' is the Entry point
+ *
+ * 'Return' is 0 (to depict end of execution, that is the program was compiled successfully)
+ */
+
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
