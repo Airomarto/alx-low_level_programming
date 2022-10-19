9bd5eda0382222d534a0b0752ce9e36044c1b55a
Airomarto <airomarto@gmail.com>
2022-10-19 23:12:10 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 97;
+
+	while (x <= 122)
+	{
+		if (x != 113 && x != 101)
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
