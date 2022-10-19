f95cd20f660722ca6eb41d7483c65fe0a1fe942d
Airomarto <airomarto@gmail.com>
2022-10-19 14:08:44 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: 0 to depict execution has occured
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return(0);
+}
