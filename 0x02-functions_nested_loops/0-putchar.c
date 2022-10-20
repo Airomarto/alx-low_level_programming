e58b82bdbd31c9d2f411465d499ff58382bc0b73
Airomarto <airomarto@gmail.com>
2022-10-20 02:03:21 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 nclude "main.h"
 /**
- * main - Entry point
+ * main - print _putchar
  *
  * Return: Always 0 (Success)
  */
@@ -9,12 +9,12 @@ int main(void)
 	_putchar("_");
 	_putchar("p");
 	_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar('\n');
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
 
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _is alpha(int c);
+int print_sign(int n);
+int _abs(int);
+
+#endif
