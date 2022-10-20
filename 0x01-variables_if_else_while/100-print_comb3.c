b36f692d6c70f9d093e1037b6ee05beb6bfdf939
Airomarto <airomarto@gmail.com>
2022-10-20 00:04:07 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+nclude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = i + 1;
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+																		{
+				putchar(i);
+				putchar(j);
+																			if (i == '8' && j == '9')
+																			{
+					putchar('\n');
+				}
+																			else
+				{
+																				putchar(',');
+					putchar(' ');
+				}
+			}															j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	return (0);
+}
