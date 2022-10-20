1a63810d01d8915c6e752540c28f5b03ce6f6725
Airomarto <airomarto@gmail.com>
2022-10-20 00:57:12 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	char c[] = "_putchar";
+	char a[] = "_putchar";
 	int b;
 	
-	for (i = 0; i < 8; i++)
-	_putchar(c[i]);
+	for (b = 0; b < 8; b++)
+	_putchar(a[b]);
 	_putchar('\n');
 	return (0);
 }
