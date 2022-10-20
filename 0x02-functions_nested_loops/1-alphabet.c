c6020e2f232da3bcbddf02fa2a01a2787d5e8bf3
Airomarto <airomarto@gmail.com>
2022-10-20 02:27:27 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char c;
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
