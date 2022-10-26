a450497a7c8ab4ed8263044303924696e9482175
Airomarto <airomarto@gmail.com>
2022-10-26 01:29:03 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * more_numbers - print 10 times the numbers
+  * from 0 to 14
+  * Return: list of numbers
+  */
+
+void more_numbers(void)
+{
+	int i;
+	int a;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			if (a > 9)
+			{
+				_putchar('0' + (a / 10));
+			}
+			_putchar('0' + (a % 10));
+		}
+		_putchar('\n');
+	}
+}
