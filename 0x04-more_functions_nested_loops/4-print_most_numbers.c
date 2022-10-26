adc5d1604359d9e8bbcab850bc36641f1c280da8
Airomarto <airomarto@gmail.com>
2022-10-26 00:50:59 +0000

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints numbers from 0 t0 9 excluding 2 and 4
+  * Return: the specified list of numbers
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + 1);
+		}
+	}
+	_putchar('\n');
+}
