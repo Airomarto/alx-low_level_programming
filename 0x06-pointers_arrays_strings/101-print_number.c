28e72539297281fb4d88e327838b0112898e2e63
Airomarto <airomarto@gmail.com>
2022-10-28 23:25:03 +0000

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,22 +4,19 @@
   * @n: number to print
   * Return: 0
   */
-
 void print_number(int n)
 {
 	int i, j, k, l;
-	
+
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-
 	i = n / 1000000000;
 	j = (n / 1000000) % 1000;
 	k = (n / 1000) % 1000;
 	l = n % 1000;
-
 	if (i > 0)
 	{
 		_putchar(i + '0');
@@ -27,7 +24,6 @@ void print_number(int n)
 		_putchar((j / 10) % 10 + '0');
 		_putchar(j % 10 + '0');
 	}
-
 	if (k > 0)
 	{
 		if (i > 0)
@@ -45,9 +41,9 @@ void print_number(int n)
 		{
 		_putchar(',');
 		_putchar(' ');
-	}
+		}
 	_putchar(l / 100 + '0');
 	_putchar((l / 10) % 10 + '0');
 	_putchar(l % 10 + '0');
-}
+	}
 }
