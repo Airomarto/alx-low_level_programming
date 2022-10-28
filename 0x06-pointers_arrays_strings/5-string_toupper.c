4a4b4969e29f7834f7c0b8a479a8b0139ed60182
Airomarto <airomarto@gmail.com>
2022-10-28 21:40:17 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to be changed
+ * Return: string in uppercase letters
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+	return (s);
+}
