d5fc9e0d7579ac8bec273e3c00d8592afdb21fe3
Airomarto <airomarto@gmail.com>
2022-10-28 22:59:00 +0000

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,20 +1,32 @@
 #include "main.h"
 /**
-  * *rot13 - to rotate by 13 places
-  * @s: string to be rotated
-  * Return: to return a string
-  */
+ * *rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: encoded string
+ */
 char *rot13(char *s)
 {
-	int i;
+	int x[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
 
-	for (i = 0; s[i]; i++)
+	int y[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
+		, 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'
+		, 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+		, 'I', 'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'n'))
-			s[i] += 13;
-		else if ((s[i] > 'm' && s[i] <= 'z') || (s[i] >= 'M' && s[i] <= 'Z'))
-			s[i] -= 13;
+		j = 0;
+		while (x[j] != s[i] && x[j] != '\0')
+			j++;
+		if (x[j] == s[i])
+			s[i] = y[j];
 	}
 	return (s);
 }
-
